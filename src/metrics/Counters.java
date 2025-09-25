588051ca4aa01fa714465ba53f13f2c7efc3735e
bekontaii <241870@astanait.edu.kz>
2025-09-25 23:05:01 +0500

    Files

---
+++ src/metrics/Counters.java
@@ -0,0 +1,28 @@
+package metrics;
+
+
+public class Counters {
+    public long comparisons = 0;
+    public long swaps = 0;
+    public long allocations = 0;
+    public int maxDepth = 0;
+    private int curDepth = 0;
+
+    public void enter() { // вход в рекурсивную функцию
+        curDepth++;
+        if (curDepth > maxDepth) maxDepth = curDepth;
+    }
+
+    public void exit() { // выход из рекурсивной функции
+        curDepth--;
+    }
+
+    public void incComparisons() { comparisons++; }
+    public void incSwaps() { swaps++; }
+    public void incAllocations() { allocations++; }
+
+    public void reset() {
+        comparisons = swaps = allocations = 0;
+        curDepth = maxDepth = 0;
+    }
+}
